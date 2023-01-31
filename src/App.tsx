6eb720de25fd4a4929a250cacb4f92385b7ca80a
MohamedDanis <danishmanu17@gmail.com>
2023-01-31 23:01:29 +0530

    updated kanban

--- src/App.tsx
+++ src/App.tsx
@@ -17,18 +17,48 @@ function App() {
             </svg>
 
           </div>
-          <div className="cards">
-            <div className="card w-full bg-base-100 shadow-xl">
+          <div className="cards ">
+            <div className="card w-full bg-slate-50 shadow-xl relative mt-4">
+              <div className="card-actions justify-end relative">
+
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 absolute m-2 cursor-pointer" fill="black" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
+
+              </div>
               <div className="card-body">
-                <h2 className="card-title">
-                  Hero Secion
+
+                <h2 className="card-title">Hero Section
                   <div className="badge badge-secondary">NEW</div>
+
                 </h2>
                 <p>Navbar and CTA</p>
                 <div className="card-actions justify-end">
-                  <div className="badge badge-outline bg-purple-700 text-white font-semibold">Front-End</div>
+                  <div className="badge badge-outline bg-violet-700 text-white">Front-End</div>
+
                 </div>
+
               </div>
+
+            </div>
+            <div className="card w-full bg-slate-50 shadow-xl relative mt-4">
+              <div className="card-actions justify-end relative">
+
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 absolute m-2 cursor-pointer" fill="black" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
+
+              </div>
+              <div className="card-body">
+
+                <h2 className="card-title">Hero Section
+                  <div className="badge badge-secondary">NEW</div>
+
+                </h2>
+                <p>Navbar and CTA</p>
+                <div className="card-actions justify-end">
+                  <div className="badge badge-outline bg-green-800 text-white">Back-End</div>
+
+                </div>
+
+              </div>
+
             </div>
           </div>
         </div>
